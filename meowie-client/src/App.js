ee8a21ca657e14f38d2b32f7194bd49460f46841
Yusuf Kalyoncu <yusufkalyoncuu@gmail.com>
2023-03-19 14:29:24 +0300

    Meowie Website

---
+++ meowie-client/src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Navbar/> 
+      <Routes>
+        <Route path="/" element={<Home/>}/> 
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ meowie-client/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='w-full h-[550px] text-white'>
+      
+    </div>
+  )
+}
+
+export default Main

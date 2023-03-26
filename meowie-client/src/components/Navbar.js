03b669d652b44ab81637b50e06fa2e69cd74ac28
Yusuf Kalyoncu <yusufkalyoncuu@gmail.com>
2023-03-26 16:48:11 +0300

    added login and register pages

--- meowie-client/src/components/Navbar.js
+++ meowie-client/src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+      <Link to="/">
       <h1 className='text-orange-600 text-4xl font-bold cursor-pointer'>MEOWIE</h1>
+      </Link>
       <div>
+        <Link to="/login">
         <button className=' pr-4 px-6 py-4 rounded cursor-pointer text-white'>Sign In</button>
+        </Link>
+        <Link to="/register">
         <button className='bg-orange-600 px-6 py-4 rounded cursor-pointer text-white'>Sign Up</button>
+        </Link>
       </div>
     </div>
   )

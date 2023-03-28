ba3345e6f4f47c2b8b89c339f0ebcdc3d00d1849
Yusuf Kalyoncu <yusufkalyoncuu@gmail.com>
2023-03-29 02:52:15 +0300

    updated login and register pages

--- meowie-client/src/pages/Login.js
+++ meowie-client/src/pages/Login.js
@@ -6,8 +6,8 @@ const Login = () => {
     <div className='w-full h-screen'>
       <img className='hidden sm:block absolute w-full h-full object-cover' alt="" src='https://wallpaper.dog/large/5543708.jpg'/>
       <div className='absolute bg-black/60 fixed top-0 left-0 w-full h-full'></div>
-      <div className='fixed w-full px-4 py-24 z-50'>
-        <div className='max-w-[450px] h-[600px] mx-auto bg-black/80 text-white'>
+      <div className='fixed w-full px-4 py-24 z-50 mt-10'>
+        <div className='max-w-[450px] h-[500px] mx-auto bg-black/80 text-white rounded'>
           <div className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>LOGIN</h1>
             <form className='w-full flex flex-col py-4'>
--- meowie-client/src/pages/Register.js
+++ meowie-client/src/pages/Register.js
@@ -5,9 +5,9 @@ const Register = () => {
     <>
       <div className='w-full h-screen'>
         <img className='hidden sm:block absolute w-full h-full object-cover' alt="" src='https://wallpaper.dog/large/5543708.jpg'/>
-        <div className='absolute bg-black/60 fixed top-0 left-0 w-full h-full'></div>
-        <div className='fixed w-full px-4 py-24 z-50'>
-          <div className='max-w-[450px] h-[600px] mx-auto bg-black/80 text-white'>
+        <div className='absolute bg-black/60 top-0 left-0 w-full h-full'></div>
+        <div className='fixed w-full px-4 py-24 z-50 mt-10'>
+          <div className='max-w-[450px] h-[600px] mx-auto bg-black/80 text-white rounded'>
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>REGISTER</h1>
               <form className='w-full flex flex-col py-4'>

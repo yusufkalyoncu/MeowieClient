7f3db872c76bd302e284e9ec6e81bc1837e41a02
Yusuf Kalyoncu <yusufkalyoncuu@gmail.com>
2023-04-08 18:41:28 +0300

    updated home page and others

---
+++ meowie-client/src/components/JoinUs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { MuiButton } from './materialui/MuiButton'
+
+const JoinUs = () => {
+  return (
+    <>
+    <div className=' w-full h-auto justify-center text-center pb-10'>
+        <h2 className='text-2xl md:text-4xl font-bold'>
+            Rate the movies you've watched<br/>
+            Save what you plan to watch<br/>
+            Share your lists with followers 
+        </h2>
+        <MuiButton className='w-48 md:w-60'>Join Us</MuiButton>
+    </div>
+    </>
+  )
+}
+
+export default JoinUs
